fd090d1e3db567cd064c25a6cf9df2ab795b8698
Kolesnykov Roma <kolesnykov.roma@gmail.com>
2021-02-13 23:33:27 +0200

    FIX: Fix typo

--- app/Models/Astrologer.php
+++ app/Models/Astrologer.php
@@ -24,7 +24,7 @@ class Astrologer extends Model
      */
     public function services(): BelongsToMany
     {
-        return $this->belongsToMany(Service::class)->withPivot('prices');
+        return $this->belongsToMany(Service::class)->withPivot('price');
     }
 
     /**
